a25c14a44b8536100df44ab4a997c81fd76e4a77
Kuzma Leshakov <leshakov.kuzma@clover.global>
2019-05-09 19:24:32 +0300

    Add lec2 Advanced SO solution

--- lec2/Advanced.scala
+++ lec2/Advanced.scala
@@ -42,5 +42,29 @@ object Advanced extends App {
   println(s"res0 = $res0")
   println(s"res1 = $res1")
 
+
+  // second solution with help of SO https://stackoverflow.com/questions/56063547/sum-of-int-elements-in-list-and-vector-using-single-function-in-scala
+
+//sealed abstract class Addable[A] {
+//def sum(el: Seq[A]): A
+//}
+
+//class MyAddable[A: Numeric]() extends Addable[A] {
+//override def sum(el: Seq[A]): A = {
+//el.sum
+//}
+//}
+
+//val myvec = Vector(1, 2, 3)
+//val mylist = List(1, 2, 3)
+
+//val inst = new MyAddable[Int]()
+
+//val res0 = inst.sum(mylist)
+//val res1 = inst.sum(myvec)
+
+//println(s"res0 = $res0")
+//println(s"res1 = $res1")
+
 }
 
